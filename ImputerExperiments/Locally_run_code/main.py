6cbdd24232d2d4107a6758075d21ccacfe6a4d96
gketronDS <gabriel.ketron@cshs.org>
2024-08-20 18:08:08 -0700

    32 and 198 running

--- ImputerExperiments/Locally_run_code/main.py
+++ ImputerExperiments/Locally_run_code/main.py
@@ -143,7 +143,7 @@ def main():
                           42545, 42225, 42712, 287, 42688, 23515]
     '''
     #classification_id_list = [30]
-    regression_id_list = [197]
+    regression_id_list = [198]
 
     
     print('starting loops')
