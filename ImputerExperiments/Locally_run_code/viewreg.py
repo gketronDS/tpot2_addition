b578f16bc6776c54ced7a045a2f631971559fec4
gketronDS <gabriel.ketron@cshs.org>
2024-10-10 11:40:21 -0700

    Classification begins

--- ImputerExperiments/Locally_run_code/viewreg.py
+++ ImputerExperiments/Locally_run_code/viewreg.py
@@ -5,8 +5,8 @@ import traceback
 
 dflist=[]
 redolist={}
-#'189', '197', '198', '215', '216', '218', '1193', '1199', '1200', '1213', '42183', '42225', '42712',  '287', '42688'
-for taskid in ['216', '1200', '1213', '42225']:
+#'189', '197', '198', '215', '216', '218', '1193', '1199', '1200', '1213', '42183', '42225', '42712',  '287', '42688', '23515'
+for taskid in ['189', '197', '198', '215', '216', '218', '1193', '1199', '1200', '1213', '42183', '42225', '42712',  '287', '42688', '23515']:
     fileoutput = '/common/ketrong/tpotexp/tpot2/ImputerExperiments/data/r/'
     csvout = pd.DataFrame(columns=['DatasetID','Exp_Name','Condition', 'Level', 'Triplicate','Exp1ImputeRMSEAcc','Exp2ImputeModel','Exp2train_explained_var','Exp2train_r2', 
                                             'Exp2train_rmse', 'Exp2ori_explained_var', 'Exp2ori_r2', 'Exp2ori_rmse', 'Exp2impute_explained_var', 'Exp2impute_r2', 
